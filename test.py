e396db85566c3dc9ccd1ab8c815704f945610bac
ok Noronha <ok.noronha.kevin@gmail.com>
2022-06-14 03:02:43 +0530

    v0.2.5

--- test.py
+++ test.py
@@ -1,46 +1,40 @@
-from tkinter import*
-import tkinter
+SELECT code FROM courses_cpy WHERE usrid=0 AND code NOT IN ('LUNCH','MEET','FREE') AND hours>0 ORDER BY random()+dest col LIMIT 1
 
-root = Tk()
-root.geometry('900x500')
+SELECT code FROM courses_cpy WHERE hours>0
 
+UPDATE jobs
+SET code = (SELECT co FROM comp(0,0) WHERE co NOT IN ('LUNCH','MEET','FREE'))
+WHERE code IS NULL AND usrid=0 AND hour IN (11,21,31,41,51)
 
-var_a = DoubleVar()
-var_b = DoubleVar()
-
-############# CREATING PANELS #####################
-#----------- General Panel  --------------#
-panel_1 = PanedWindow(bd=4,orient = HORIZONTAL ,relief="raised")#, bg = "red")
-panel_1.pack(fill=BOTH, expand=1)
-#----------- Fist Panel  --------------#
-panel_3 = PanedWindow(panel_1, orient = VERTICAL, relief="raised")#, bg = "yellow")
-panel_1.add(panel_3, minsize=200) #inserting on panel_1
-#----------- Second Panel  --------------#
-panel_2 = PanedWindow(panel_1, orient = VERTICAL, relief="raised")#, bg = "blue")
-panel_1.add(panel_2, minsize=800) #inserting on panel_1
+UPDATE jobs
+SET code = (SELECT code FROM courses_cpy WHERE usrid=0 AND code NOT IN ('LUNCH','MEET','FREE') AND hours>0 ORDER BY random()+dest_col LIMIT 1)
+WHERE code IS NULL AND usrid=0 AND hour IN (11,21,31,41,51)
 
 
-label2=Label(panel_3,text="Pass the cursor below me")
-panel_3.add(label2)
+CREATE OR REPLACE FUNCTION comp (i NUMERIC(12),imp NUMERIC(2))
+    RETURNS TABLE (
+        co VARCHAR(8),
+        hrs INTEGER,
+        ids NUMERIC (12)
+)
+AS $$
+BEGIN
+    RETURN QUERY SELECT * FROM courses_cpy WHERE usrid=i AND hours>0 ORDER BY random() LIMIT 1;
+END; $$
 
-textbox2=Scale(panel_3,orient=HORIZONTAL,variable = var_a)
-panel_3.add(textbox2)
+LANGUAGE 'plpgsql';
 
-label4=Label(panel_3,text="Pass the cursor above me too")
-panel_3.add(label4)
+CREATE OR REPLACE FUNCTION first (i NUMERIC(12),imp NUMERIC(2))
+RETURNS INTEGER
+AS
+$$
+BEGIN
+    UPDATE jobs
+    SET code = (SELECT co FROM comp(i,imp) WHERE co NOT IN ('LUNCH','MEET','FREE'))
+    WHERE code IS NULL AND usrid=i AND hour IN (11,21,31,41,51);
+    RETURN 0;
+END;
+$$
+LANGUAGE 'plpgsql';
 
-textbox4=Scale(panel_3,orient=HORIZONTAL,variable = var_b)
-panel_3.add(textbox4)
-
-
-
-def bla():
-    pass
-button1 = Button(panel_3,text="Why I have this size?", height = 0, width = 0, command= bla())
-panel_3.add(button1)
-button1.pack(fill=X, expand=1) # Only fits in X (horizontal direction), expands according to the panel
-
-
-
-
-tkinter.mainloop()
+PERFORM first(0,0);
